ef93d74f296ebbde171c7bbaa9853d4d696a1339
Quan Dang <18520339@gm.uit.edu.vn>
2020-09-07 20:03:34 +0700

    Update OAuthApps.jsx

--- src/config/OAuthApps.jsx
+++ src/config/OAuthApps.jsx
@@ -2,10 +2,9 @@
 /* eslint-disable */
 'use strict';
 
-export const FACEBOOK_APP_ID = '203973034277249';
-export const GITHUB_CLIENT_ID = '7c3e8be1ef8c7471cb7a';
-export const GITHUB_CLIENT_SECRET = 'd96d5576ea18b7241c2986568a9852ee4b5edd9c';
-export const GOOGLE_CLIENT_ID =
-	'175404929285-flbnqbt6ph4r6pqtqt2398p9useulv00.apps.googleusercontent.com';
+export const FACEBOOK_APP_ID = '';
+export const GITHUB_CLIENT_ID = '';
+export const GITHUB_CLIENT_SECRET = '';
+export const GOOGLE_CLIENT_ID = '';
 
 /* eslint-enable */
