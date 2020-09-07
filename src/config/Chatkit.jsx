45c2993979d693dacaccc1880a03d6c6d1356c66
Quan Dang <18520339@gm.uit.edu.vn>
2020-09-07 20:04:26 +0700

    Update Chatkit.jsx

--- src/config/Chatkit.jsx
+++ src/config/Chatkit.jsx
@@ -2,10 +2,8 @@
 /* eslint-disable */
 'use strict';
 
-export const instanceLocator = 'v1:us1:caf4a583-8194-4510-adbe-c9879f060ffc';
-export const tokenUrl =
-	'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/caf4a583-8194-4510-adbe-c9879f060ffc/token';
-export const key =
-	'7ea5e986-72fe-4cec-ba0c-4d07252f5f5f:LUB9fZZjg7vdpaFZlJEPBbZfKAe7sPLr/fhtA1+c3Co=';
+export const instanceLocator = '';
+export const tokenUrl = '';
+export const key = '';
 
 /* eslint-enable */
