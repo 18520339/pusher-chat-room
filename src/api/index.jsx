a8a9aa184b450d188718e501d8b55cc0471be455
Quan Dang <18520339@gm.uit.edu.vn>
2020-09-08 08:20:23 +0700

    Update index.jsx

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -2,15 +2,14 @@
 /* eslint-disable */
 'use strict';
 
-export const SWRTC_API_KEY = '970b4e5a23e7c4400327a45d';
+export const SWRTC_API_KEY = '';
 export const SWRTC_API_URL = `https://api.simplewebrtc.com/config/guest/${SWRTC_API_KEY}`;
 
-export const NEWS_API_KEY = 'e30c5be02d0b4698b6a7722d93c70225';
+export const NEWS_API_KEY = '';
 export const NEWS_API_URL = 'https://newsapi.org/v2/everything';
 
 export const AVATAR_API_URL = `https://avatars.dicebear.com/v2`;
-export const AVATAR_OPTIONS =
-	'options[eyes][]=squint&options[eyebrow][]=raised&options[mouth][]=smile';
+export const AVATAR_OPTIONS = 'options[eyes][]=squint&options[eyebrow][]=raised&options[mouth][]=smile';
 
 export const PROXY_URL = 'https://cors-anywhere.herokuapp.com';
 export const GITHUB_ACCESS_API = 'https://api.github.com/user';
